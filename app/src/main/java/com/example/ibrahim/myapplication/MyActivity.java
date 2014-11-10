596b574f140596b607c318f1bb4d521c69262db4
Ibrahimox <ibrahim_aweidah@hotmail.com>
2014-11-10 18:36:48 +0100

    test2

--- app/src/main/java/com/example/ibrahim/myapplication/MyActivity.java
+++ app/src/main/java/com/example/ibrahim/myapplication/MyActivity.java
@@ -11,7 +11,9 @@ public class MyActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);    }
+        setContentView(R.layout.layout);
+    //Test
+    }
 
 
     @Override
